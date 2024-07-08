1fc5d4105e0ce4431b3fe61058ff7b213ddba307
Karyl <62021442+lenlenlL6@users.noreply.github.com>
2024-07-08 13:56:24 +0700

    Update XuidHelper.php

--- src/LootSpace369/VaniMMo/utils/XuidHelper.php
+++ src/LootSpace369/VaniMMo/utils/XuidHelper.php
@@ -1,1 +1,18 @@
+<?php
 
+namespace LootSpace369\VaniMMo\utils;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+
+class XuidHelper {
+  
+  public static function getPlayerByXuid(string $xuid) : ?Player {
+    foreach(Server::getInstance()->getOnlinePlayers() as $player) {
+      if($player->getXuid() === $xuid) {
+        return $player;
+      }
+    }
+    return null;
+  }
+}
