cae88c269108ed3b02c66fece2fbc551b689e420
Karyl <62021442+lenlenlL6@users.noreply.github.com>
2024-07-08 17:41:51 +0700

    Update Loader.php

--- src/LootSpace369/VaniMMo/Loader.php
+++ src/LootSpace369/VaniMMo/Loader.php
@@ -16,10 +16,16 @@ namespace LootSpace369\VaniMMo;
 
 use pocketmine\plugin\PluginBase;
 use vennv\vapm\{VapmPMMP,System};
+use LootSpace369\VaniMMo\pet\PetSql;
 
 class Loader extends PluginBase {
 
   public function onEnable(): void {
     VapmPMMP::init($this);
+    PetSql::init($this->getDataFolder());
+  }
+
+  public function onDisable() : void {
+    PetSql::close();
   }
 }
